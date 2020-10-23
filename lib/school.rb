5051e5ec87863bad86d3a2a126198e6f21e79879
Nathaniel-Kerr <NathanielKerr10@gmail.com>
2020-10-22 20:48:19 -0700

    commit tree fc58805e1071b10dba49d53e46bf043332051c03

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    attr_accessor :roster, :name, :grade
+    attr_reader :school
+
+def initialize(school)
+    @school = school
+    @roster = {}
+    end
+
+def add_student(name, grade) 
+    if !@roster[grade]
+        @roster[grade] = [name]
+    elsif @roster[grade] << name
+    end
+end
+
+def grade(grade)
+    @roster.select do |key, value|
+        if key == grade
+           return value
+end
+end
+end
+
+def sort
+    @roster.each_value(&:sort!) 
+end
+end
